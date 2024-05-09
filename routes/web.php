c81346fb86be83fe8af69c1754114a95c31f1a49
Engkokk <eastdifferent@gmail.com>
2024-05-09 16:27:26 +0700

    Add existing file

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SessionController::class,'index']);
 
-Route::get('session/put','App\Http\Controllers\SessionController@store');
-Route::get('session/get','App\Http\Controllers\SessionController@index');
-Route::get('session/push','App\Http\Controllers\SessionController@push');
-Route::get('session/del','App\Http\Controllers\SessionController@delete');
+class SessionController extends Controller
+{
+public function index(){
+    echo '<ul>';
+    echo '<li><a href=/buat-session>Buat Session</a></li>';
+    echo '<li><a href=/akses-session>Akses Session</a></li>';
+    echo '<li><a href=/hapus-session>Hapus Session</a></li>';
+    echo '<li><a href=/flash-session>Flash Session</a></li>';
+    echo '</ul>';
+    }
+}
